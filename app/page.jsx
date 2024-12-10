0ffa909ca6035ab1f48e7daf9265103b4e8bdbf6
jsoons <jsoons@gmail.com>
2024-12-10 11:36:28 +0100

    First content on page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center">AI-Powered Promts</span>
+        <p className="desc text-center">
+          Promtopia is a open-source AI prompting tool for modern world to discover, create and share creative prompts.
+        </p>
+        
+        {/* Feed */}
+
+        
+      </h1>
+    </section>
   )
 }
 
