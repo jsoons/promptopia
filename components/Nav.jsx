f871530dfc0a4edc23891678a977e5706715923f
jsoons <jsoons@gmail.com>
2024-12-16 17:24:54 +0100

    getProvide

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,12 @@ import { signIn, signOut, useSession, getProviders} from 'next-auth/react'
 const Nav = () => {
   const isUserLoggedIn = true
   const [providers, setProviders] = useState(null)
+
+  useEffect(()=> {
+    const setProviders = async () => {
+      const response = await getProviders()
+    }
+  }, [])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
         <Link href="/ "className="flex gap-2 flex-center">
